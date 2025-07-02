13f9d6068cd49b8a3a503fd606d2ccafa10d959a
unknown <shubhgupta4102@gmail.com>
2025-07-02 23:44:21 +0530

    bug fixed

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,7 +12,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
     <html lang="en">
       <body className=" ">
         <SidebarWrapper />
-        <main className="">
+        <main className="ml-64 min-h-screen">
           {children}
         </main>
       </body>
