c3a747abb45998eef3902e3843ba0bc438e40a41
Tanisha Bhatt <tanisha2310187@akgec.ac.in>
2026-07-01 23:23:44 +0530

    Update axios base URL for API requests

--- app/lib/axios.ts
+++ app/lib/axios.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 import { getAccessToken, refreshAccessToken, removeTokens } from '../utils/token';
 
 const axiosInstance = axios.create({
-  baseURL: 'https://project2-zphf.onrender.com/api/auth',
+  baseURL: 'https://bhattanisha.me/api/auth',
   headers: { 'Content-Type': 'application/json' },
 });
 
